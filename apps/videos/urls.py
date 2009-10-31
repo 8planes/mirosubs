1223128fd88ed5eeca6c6fdb269ec0389d90d913
Rodrigo Guzman <rodguze@gmail.com>
2009-10-31 05:49:12 -0500

    Made some of the links on base.html and index.html live.
    	modified:   apps/videos/urls.py
    	modified:   templates/base.html
    	modified:   templates/index.html

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^create/$', 'videos.views.create' ,name='index'),
+    url(r'^create/$', 'videos.views.create', name='create'),
     url(r'(?P<video_id>(\w|-)+)/$', 'videos.views.video', name='video'),
 
 )
