dc8eec155811304ab8b92c28ac65d4c92ab5f9b4
Rodrigo Guzman <rodguze@gmail.com>
2009-11-01 01:24:27 -0500

    Fixed the video models.
    	modified:   apps/widget/models.py

--- apps/widget/models.py
+++ apps/widget/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Video(models.Model):
     video_id = models.CharField(max_length=255)
     video_url = models.CharField(max_length=2048, unique=True)
     view_count = models.PositiveIntegerField(default=0)
     # the person who was first to start captioning this video.
-    owner = django.contrib.auth.models.User()
+    owner = models.ForeignKey(User)
     # always set to False for the time being.
     allow_community_edits = models.BooleanField()
     writelock_time = models.DateTimeField(null=True)
-    writelock_owner = django.contrib.auth.models.User(null=True)
+    writelock_owner = models.ForeignKey(User, null=True, related_name='writelock_owners')
 
     def __unicode__(self):
         return self.video_url
@@ -26,7 +27,7 @@ class VideoCaptionVersion(models.Model):
     # true iff all captions have beg & end time values assigned.
     is_complete = models.BooleanField()
     datetime_started = models.DateTimeField()
-    user = django.contrib.auth.models.User()
+    user = models.ForeignKey(User)
 
 class VideoCaption(models.Model):
     version = models.ForeignKey(VideoCaptionVersion)
