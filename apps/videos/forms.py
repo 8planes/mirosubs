be78bb8b9a730d60c856c73c9481e69644267676
Rodrigo Guzman <rodguze@gmail.com>
2009-11-01 06:40:40 -0600

    Updated views and form to handle new model.
    	modified:   apps/videos/forms.py
    	modified:   apps/videos/views.py
    	apps/videos/admin.py

--- apps/videos/forms.py
+++ apps/videos/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 from widget.models import Video
 
-class VideoForm(forms.Form):
-    video_url = forms.URLField()
+class VideoForm(forms.ModelForm):
+    class Meta:
+        model = Video
+        exclude = ('video_id', 'view_count', 'owner',
+                   # TODO: this is being set to false in the save method it 
+                   # should just be in the form when the models are finished.
+                   'allow_community_edits',
+                   'writelock_time', 'writelock_owner',)
+                   
--- apps/videos/views.py
+++ apps/videos/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.generic.list_detail import object_list
@@ -14,11 +14,17 @@ def create(request):
     if request.method == 'POST':
         video_form = VideoForm(request.POST)
         if video_form.is_valid():
-            video, created = Video.objects.get_or_create(video_url=video_form.cleaned_data['video_url'])
+            video, created = Video.objects.get_or_create(
+                                video_url=video_form.cleaned_data.get('video_url'),
+                                defaults={'owner': request.user})
             if created:
+                # TODO: log to activity feed
                 pass
-                # log into activity feed
-            return HttpResponseRedirect(reverse('videos:video', kwargs={'video_id':video.video_id}))
+            if video.owner == request.user or video.allow_community_edits:
+                return HttpResponseRedirect(reverse('videos:video', kwargs={'video_id':video.video_id}))
+            else:
+                # TODO: better error page?
+                return HttpResponse('You are not allowed to add transcriptions to this video.')
     else:
         video_form = VideoForm()
     return render_to_response('videos/create.html', locals(),
@@ -29,7 +35,7 @@ def video(request, video_id):
     video = get_object_or_404(Video, video_id=video_id)
     video.view_count += 1
     video.save()
-    has_subtitles = video.videocaption_set.count() > 0
+    has_subtitles = video.videocaptionversion_set.count() > 0
     has_subtitles_var = 'true' if has_subtitles else 'false'
     uuid = str(uuid4()).replace('-', '')
     return render_to_response('videos/video.html', add_params(locals()),
