b8680797aa9b37ffadfb1c15eaf27d78030ab063
Rodrigo Guzman <rodguze@gmail.com>
2009-11-03 17:43:52 -0600

    First stab at the helper function to handle all the widget js context variables.
    	modified:   apps/widget/__init__.py

---
+++ apps/widget/__init__.py
@@ -0,0 +1,28 @@
+from uuid import uuid4
+from videos import models
+from django.conf import settings
+from django.contrib.sites import Site
+
+def js_context(request, video):
+    params = {'uuid': uuid4().replace('-', ''),
+              'video_id': video.video_id,
+              'video_url': video.video_url,}
+    if video.caption_state == models.NO_CAPTIONS:
+        params['show_tab'] = 0
+    elif video.caption_state == models.CAPTIONS_IN_PROGRESS:
+        if request.user.is_authenticated and request.user == video.owner:
+            params['show_tab'] = 1
+        else:
+            params['show_tab'] = 2
+            params['owned_by'] = video.owner.username
+    else:
+        params['show_tab'] = 3
+    params["js_use_compiled"] = settings.JS_USE_COMPILED
+    if settings.JS_USE_COMPILED:
+        # might change in future when using cdn to serve static js
+        params["js_dependencies"] = [full_path("mirosubs-compiled.js")]
+    else:
+        params["js_dependencies"] = [full_path(js_file) for js_file in settings.JS_RAW]
+    params["site"] = Site.objects.get_current()
+    return params
+    
