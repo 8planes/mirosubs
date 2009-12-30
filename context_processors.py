7dd357ab67544a8f0f434fe5e1eb6044a5e8c9fa
Rodrigo Guzman <rodguze@gmail.com>
2009-12-30 13:54:56 +0000

    Context processor to make the current site always available to temmplates.
    	new file:   context_processors.py
    	modified:   settings.py

---
+++ context_processors.py
@@ -0,0 +1,8 @@
+from django.contrib.sites.models import Site
+
+def current_site(request):
+    try:
+        return { 'current_site':Site.objects.get_current() }
+    except Site.DoesNotExist:
+        return { 'current_site': }
+
--- settings.py
+++ settings.py
@@ -85,6 +85,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'mirosubs.context_processors.current_site',
 )
 
 INSTALLED_APPS = (
