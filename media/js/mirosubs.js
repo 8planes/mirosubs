fd82d526b65138de40d3cf13f08135c42185d94d
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2010-01-04 23:41:23 -0200

    * handle video_url parameter in embed code * load html5 video player * add "subtitle this video" link * remove addCaption interface, we'll do it internally * load a dialog for the transcription widget (TODO: actually integrate the widget)

--- media/js/mirosubs.js
+++ media/js/mirosubs.js
@@ -28,6 +28,18 @@ mirosubs.twitter_login = function(twitter_login_url) {
         }, 1000);
 };
 
+mirosubs.transcribe = function(transcribe_url) {
+    var html = ['<div><iframe marginwidth="0" marginheight="0" hspace="0" ',
+                'vspace="0" frameborder="0" allowtransparency="true" src="', 
+                transcribe_url,
+                '"</iframe></div>'].join('');
+    var dialog = new goog.ui.Dialog("mirosubs-modal-dialog", true);
+    dialog.setButtonSet(null);
+    dialog.setContent(html);
+    dialog.setTitle("Transcribe Video");
+    dialog.setVisible(true);
+};
+
 // see http://code.google.com/closure/compiler/docs/api-tutorial3.html#mixed
 window["mirosubs"] = mirosubs;
 mirosubs["login"] = mirosubs.login;
