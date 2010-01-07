6464a5f38866ec2cca73026414cbf3027a02976b
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2010-01-07 01:08:41 -0200

    adding my settings file

---
+++ fs-settings.py
@@ -0,0 +1,24 @@
+from settings import *
+import logging
+
+SITE_ID = 4
+SITE_NAME = 'mirosubs-adam'
+
+
+# socialauth-related
+OPENID_REDIRECT_NEXT = '/accounts/openid/done/'
+
+OPENID_SREG = {"requred": "nickname, email", "optional":"postcode, country", "policy_url": ""}
+OPENID_AX = [{"type_uri": "email", "count": 1, "required": False, "alias": "email"}, {"type_uri": "fullname", "count":1 , "required": False, "alias": "fullname"}]
+
+TWITTER_CONSUMER_KEY = '6lHYqtxzQBD3lQ55Chi6Zg'
+TWITTER_CONSUMER_SECRET = 'ApkJPIIbBKp3Wph0JBoAg2Nsk1Z5EG6PFTevNpd5Y00'
+
+FACEBOOK_API_KEY = ''
+FACEBOOK_API_SECRET = ''
+
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
+                           'socialauth.auth_backends.OpenIdBackend',
+                           'socialauth.auth_backends.TwitterBackend',
+                           'socialauth.auth_backends.FacebookBackend',
+                           )
