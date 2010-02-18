8bdd7cebf4b84fa8bba55957f2acc983bdd2d345
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2010-02-18 02:04:53 -0200

    play after skipback (even if video was paused before)

--- media/js/widget/subtitle/mainpanel.js
+++ media/js/widget/subtitle/mainpanel.js
@@ -52,6 +52,7 @@ mirosubs.subtitle.MainPanel.prototype.handleKeyDown_ = function(event) {
     if (event.keyCode == goog.events.KeyCodes.CTRL) {
         var now = this.videoPlayer_.getPlayheadTime();
         this.videoPlayer_.setPlayheadTime(Math.max(now - 3, 0));
+        this.videoPlayer_.play();
     }
     if (event.keyCode == goog.events.KeyCodes.TAB){
         //TODO: this violates accessibility guidelines. Use another key instead of TAB!
