00ebab213a6dd6ddc033d88063c8aaffee6ae273
Adam Duston <adamduston@Adam-Dustons-MacBook.local>
2010-04-16 15:40:49 -0400

    Starting timeline component

---
+++ media/js/widget/timeline/timeline.js
@@ -0,0 +1,37 @@
+// Universal Subtitles, universalsubtitles.org
+// 
+// Copyright (C) 2010 Participatory Culture Foundation
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see 
+// http://www.gnu.org/licenses/agpl-3.0.html.
+
+goog.provide('mirosubs.timeline.Timeline');
+
+/**
+ *
+ * @param {number} spacing The space, in seconds, between two 
+ *     major ticks.
+ */
+mirosubs.timeline.Timeline = function(spacing) {
+    goog.ui.Component.call(this);
+    this.spacing_ = spacing;
+    this.timesRow0_ = 
+};
+mirosubs.timeline.Timeline.prototype.createDom = function() {
+    mirosubs.timeline.Timeline.superClass_.createDom.call(this);
+    
+};
+mirosubs.timeline.Timeline.prototype.setTime(time) {
+    
+};
