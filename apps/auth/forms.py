a8cd4797395d6f683a722064f10e4fa1529b9917
alerion <alerion.um@gmail.com>
2010-05-06 10:49:56 +0300

    validate email field like unique test password retrieval fix login link on password reset page
    Bug 13295

--- apps/auth/forms.py
+++ apps/auth/forms.py
@@ -11,3 +11,9 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         self.fields['email'].required = True
+        
+    def _get_unique_checks(self):
+        #add email field validate like unique
+        unique_checks, date_checks = super(CustomUserCreationForm, self)._get_unique_checks()
+        unique_checks.append(('email',))
+        return unique_checks, date_checks
