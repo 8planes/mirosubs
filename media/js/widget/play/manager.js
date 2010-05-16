a7a13f84d3ceef5b079d47ddecfd780c44eac38f
Adam Duston <adamduston@Adam-Dustons-MacBook.local>
2010-05-16 13:29:15 -0400

    Fixed subtitle display in play mode

--- media/js/widget/play/manager.js
+++ media/js/widget/play/manager.js
@@ -24,13 +24,13 @@ mirosubs.play.Manager = function(videoPlayer, captions) {
     this.captionManager_ = new mirosubs.CaptionManager(videoPlayer.getPlayheadFn());
     this.captionManager_.addCaptions(captions);
     goog.events.listen(this.captionManager_,
-                       mirosubs.CaptionManager.EventType.CAPTION,
-                       this.captionReached_, false, this);
+                       mirosubs.CaptionManager.CAPTION,
+                       this.captionReached_);
 };
 goog.inherits(mirosubs.play.Manager, goog.Disposable);
-mirosubs.play.Manager.prototype.captionReached_ = function(jsonCaptionEvent) {
-    var c = jsonCaptionEvent.caption;
-    this.videoPlayer_.showCaptionText(c ? c['caption_text'] : '');
+mirosubs.play.Manager.prototype.captionReached_ = function(event) {
+    var c = event.caption;
+    this.videoPlayer_.showCaptionText(c ? c.getCaptionText() : '');
 };
 mirosubs.play.Manager.prototype.disposeInternal = function() {
     mirosubs.play.Manager.superClass_.disposeInternal.call(this);
