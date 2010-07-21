8planes/mirosubs

commit 7176a233a53e351b7f175d4babb3c3d34c2735f7

* Now fooling widget into thinking that we're adding a new revision in null mode, for ticket http://bugzilla.pculture.org/show_bug.cgi?id=13938 . * Fixed css problem with dialog * Fixed html5 video resumeLoading error.

apps/widget/rpc.py
@@ -123,10 +123,12 @@ def start_editing_null(request, video_id, base_version_no=None):
         null_captions = video.null_captions(request.user)
         if null_captions is None:
             captions = []
+            version_no = 0
         else:
             captions = list(null_captions.videocaption_set.all())
+            version_no = 1
     return { 'can_edit': True,
-             'version': 0,
+             'version': version_no,
              'existing': [caption.to_json_dict() for
                           caption in captions] }
 

media/css/mirosubs-widget.css
@@ -29,13 +29,11 @@ top: 0;
    line-height: 16px !important;
    -moz-box-shadow: 0px 3px 10px rgba(0,0,0,9) !important;
    -webkit-box-shadow: 0 1px 3px rgba(0,0,0,01) !important;
-
    padding: px !important;
    background: #fff !important;
    position: absolute !important;
-top: 0 !important;
-border: 1px solid #000;
-border-top: 0;
+   border: 1px solid #000;
+   border-top: 0;
 }
 
 .mirosubs-modal-widget-content {

media/js/video/abstractvideoplayer.js
@@ -98,6 +98,9 @@ mirosubs.video.AbstractVideoPlayer.prototype.togglePause = function() {
 mirosubs.video.AbstractVideoPlayer.prototype.isLoadingStopped = function() {
   return this.isLoadingStopped_;  
 };
+/**
+ * @protected
+ */
 mirosubs.video.AbstractVideoPlayer.prototype.setLoadingStopped = function(isLoadingStopped) {
   this.isLoadingStopped_ = isLoadingStopped;  
 };

media/js/video/html5videoplayer.js
@@ -176,6 +176,7 @@ mirosubs.video.Html5VideoPlayer.prototype.stopLoadingInternal = function() {
 
 mirosubs.video.Html5VideoPlayer.prototype.resumeLoadingInternal = function(playheadTime) {
     this.videoElem_['src'] = this.videoSource_.getVideoURL();
+    this.setLoadingStopped(false);
     this.setPlayheadTime(playheadTime);
     this.pause();
 };

commit 1582e3db937cef670558e4310299e9df974d0dec

Fixed problem introduced in http://github.com/8planes/mirosubs/commit/7176a233a53e351b7f175d4babb3c3d34c2735f7 that prevented null widget from working for anonymous users. Should fix http://bugzilla.pculture.org/show_bug.cgi?id=13955

apps/widget/rpc.py
@@ -116,6 +116,7 @@ def start_editing(request, video_id, base_version_no=None):
 
 def start_editing_null(request, video_id, base_version_no=None):
     # FIXME: note duplication with start_editing, fix that.
+    version_no = 0
     if not request.user.is_authenticated():
         captions = []
     else:
@@ -123,7 +124,6 @@ def start_editing_null(request, video_id, base_version_no=None):
         null_captions = video.null_captions(request.user)
         if null_captions is None:
             captions = []
-            version_no = 0
         else:
             captions = list(null_captions.videocaption_set.all())
             version_no = 1