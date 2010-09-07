f1a1c0d3464224af12f94c9479e6e636b29428ee
Alerion <alerion.um@gmail.com>
2010-09-07 11:07:52 +0300

    fix user admin interface

--- apps/auth/admin.py
+++ apps/auth/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
+class CustomUserCreationForm(forms.ModelForm):
+    username = forms.RegexField(label=_("Username"), max_length=30, regex=r'^\w+$',
+        help_text = _("Required. 30 characters or fewer. Alphanumeric characters only (letters, digits and underscores)."),
+        error_message = _("This value must contain only letters, numbers and underscores."))
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput)
+    email = forms.EmailField(label=_('Email'))
+    
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+        ),
+    )
 admin.site.unregister(User)    
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
