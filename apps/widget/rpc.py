80b1572c17c1bbc98b76e96f9e4c447a0c0d09ec
Adam Duston <aduston@gmail.com>
2010-10-26 20:48:59 -0400

    Initial work on new draft system for subtitles

--- apps/widget/rpc.py
+++ apps/widget/rpc.py
@@ -45,75 +45,79 @@ class Rpc(BaseRpc):
         if not can_writelock:
             return { "can_edit": False, 
                      "locked_by" : language.writelock_owner_name }
-        version = self._get_version_for_editing(
-            request.user, language, base_version_no, fork)
-        version.last_saved_packet = 0
-        version.save()
-        subtitles = self._subtitles_dict(version)
+        draft = self._get_draft_for_editing(
+            request, language, base_version_no, fork)
+        subtitles = self._subtitles_dict(draft)
         return_dict = { "can_edit" : True,
+                        "draft_pk" : draft.pk,
                         "subtitles" : subtitles }
-        if version.is_dependent():
+        if draft.is_dependent():
             video = models.Video.objects.get(video_id=video_id)
             return_dict['original_subtitles'] = \
-                self._subtitles_dict(video.latest_finished_version())
+                self._subtitles_dict(video.latest_version())
         return return_dict
 
-    def update_lock(self, request, video_id, language_code=None):
-        language = models.Video.objects.get(
-            video_id=video_id).subtitle_language(language_code)
-        if language.can_writelock(request):
-            language.writelock(request)
-            language.save()
-            return { "response" : "ok" }
-        else:
-            return { "response" : "failed" }        
-
-    def release_lock(self, request, video_id, language_code=None):
-        language = models.Video.objects.get(
-            video_id=video_id).subtitle_language(language_code)
+    def release_lock(self, request, draft_pk):
+        language = models.SubtitleDraft.objects.get(pk=draft_pk).language
         if language.can_writelock(request):
             language.release_writelock()
             language.save()
         return { "response": "ok" }
 
-    def save_subtitles(self, request, video_id, packets, language_code=None):
-        if not request.user.is_authenticated():
-            return { "response" : "not_logged_in" }
-
-        language = models.Video.objects.get(
-            video_id=video_id).subtitle_language(language_code)
-        if not language.can_writelock(request):
+    def save_subtitles(self, request, draft_pk, packets):
+        draft = models.SubtitleDraft.objects.get(pk=draft_pk)
+        if not draft.language.can_writelock(request):
             return { "response" : "unlockable" }
-        language.writelock(request)
-        subtitle_version = language.latest_version()
-        self._save_packets(subtitle_version, packets)
+        if not draft.matches_request(request):
+            return { "response" : "does not match request" }
+        draft.language.writelock(request)
+        self._save_packets(draft, packets)
         return {"response" : "ok", 
-                "last_saved_packet": subtitle_version.last_saved_packet}
+                "last_saved_packet": draft.last_saved_packet}
 
-    def finished_subtitles(self, request, video_id, packets, language_code=None):
+    def finished_subtitles(self, request, draft_pk, packets):
+        draft = models.SubtitleDraft.objects.get(pk=draft_pk)
         if not request.user.is_authenticated():
             return { 'response': 'not_logged_in' }
-
-        from videos.models import Action
-        
-        video = models.Video.objects.get(video_id=video_id)
-        language = video.subtitle_language(language_code)
-        if not language.can_writelock(request):
+        if not draft.language.can_writelock(request):
             return { "response" : "unlockable" }
-        last_version = language.latest_version()
-        self._save_packets(last_version, packets)
-        last_version.finished = True
-        last_version.user = request.user
-        last_version.save()
-        language = models.SubtitleLanguage.objects.get(pk=language.pk)
-        language.release_writelock()
-        language.save()
+        if not draft.matches_request(request):
+            return { "response" : "does not match request" }
 
-        Action.create_caption_handler(last_version)
+        self._save_packets(draft, packets)
+
+        new_version = self._create_version_from_draft(draft, request.user)
+        if new_version.time_change > 0 and new_version.text_change > 0:
+            new_version.save()
+            new_version.language.is_forked = new_version.is_forked
+            new_version.language.release_writelock()
+            new_version.language.save()
+
+            for subtitle in models.Subtitle.objects.all():
+                print "%s,%s" % (subtitle.version, subtitle.draft)
+
+            from videos.models import Action
+            Action.create_caption_handler(new_version)
+
         return { "response" : "ok",
-                 "last_saved_packet": last_version.last_saved_packet,
+                 "last_saved_packet": draft.last_saved_packet,
                  "drop_down_contents" : 
-                     self._drop_down_contents(request.user, video) }
+                     self._drop_down_contents(
+                         request.user, draft.video) }
+
+    def _create_version_from_draft(self, draft, user):
+        version = models.SubtitleVersion(
+            language=draft.language,
+            version_no=draft.version_no,
+            is_forked=draft.is_forked,
+            datetime_started=draft.datetime_started,
+            user=user)
+        for subtitle in draft.subtitle_set.all():
+            version.subtitle_set.add(subtitle.duplicate_for(version=version))
+        print("subtitle_set count: %s" % version.subtitle_set.count())
+        version.set_changes(draft.parent_version)
+        print("2 subtitle_set count: %s" % version.subtitle_set.count())
+        return version
 
     def fetch_subtitles(self, request, video_id, language_code=None):
         video = models.Video.objects.get(video_id=video_id)
@@ -137,63 +141,56 @@ class Rpc(BaseRpc):
             current_version, deleted, inserted, updated)
         current_version.save()
 
-    def _get_version_for_editing(self, user, language, 
-                                 base_version_no=None, 
-                                 fork=False):
-        subtitle_versions = list(language.subtitleversion_set.order_by('-version_no'))
-
+    def _get_draft_for_editing(self, request, language, 
+                               base_version_no=None, 
+                               fork=False):
         if base_version_no is None:
-            version_to_copy, new_version_no = \
-                self._prepare_version_to_edit_latest(user, subtitle_versions)
-        else:
-            version_to_copy = language.version(base_version_no)
-            latest_version = language.latest_version()
-            if not latest_version.finished:
-                latest_version.delete()
-            new_version_no = language.latest_finished_version().version_no + 1
+            draft = self._find_existing_draft_to_edit(
+                request, language)
+            if draft:
+                draft.last_saved_packet = 0
+                draft.save()
+                return draft
 
-        new_version = None
-        if version_to_copy is None or new_version_no > version_to_copy.version_no:
-            new_version = models.SubtitleVersion(
-                language=language,
-                version_no=new_version_no,
-                datetime_started=datetime.now())
-            if fork or (version_to_copy is not None and 
-                        version_to_copy.is_forked):
-                new_version.is_forked = True
-                if not language.is_forked:
-                    language.is_forked = True
-                    language.save()
-            if user.is_authenticated():
-                new_version.user = user
-            new_version.save()
-            if version_to_copy is not None:
-                for subtitle in version_to_copy.subtitle_set.all():
-                    new_version.subtitle_set.add(subtitle.duplicate_for(new_version))
-        return version_to_copy if new_version is None else new_version
+        version_to_copy = language.version(base_version_no)
+        draft = models.SubtitleDraft(
+            language=language,
+            parent_version=version_to_copy,
+            datetime_started=datetime.now())
+        if fork or (version_to_copy is not None and 
+                    version_to_copy.is_forked):
+            draft.is_forked = True
+        if request.user.is_authenticated():
+            draft.user = request.user
+        draft.browser_id = request.browser_id
+        draft.last_saved_packet = 0
+        draft.save()
 
-    def _prepare_version_to_edit_latest(self, user, subtitle_versions):
-        version_to_copy = None
-        if len(subtitle_versions) == 0:
-            new_version_no = 0
-        else:
-            version_to_copy = subtitle_versions[0]
-            if version_to_copy.finished:
-                new_version_no = version_to_copy.version_no + 1
-            else:
-                if not user.is_anonymous() and \
-                        version_to_copy.user is not None and \
-                        version_to_copy.user.pk == user.pk:
-                    new_version_no = version_to_copy.version_no                    
-                elif len(subtitle_versions) > 1:
-                    version_to_copy = subtitle_versions[1]
-                    new_version_no = version_to_copy.version_no + 1
-                    subtitle_versions[0].delete()
-                else:
-                    version_to_copy = None
-                    new_version_no = 0
-                    subtitle_versions[0].delete()
-        return version_to_copy, new_version_no
+        if version_to_copy is not None:
+            for subtitle in version_to_copy.subtitle_set.all():
+                draft.subtitle_set.add(subtitle.duplicate_for(draft=draft))
+        return draft
+
+    def _find_existing_draft_to_edit(self, request, language):
+        latest_version = language.latest_version()
+        draft = None
+        if request.user.is_authenticated():
+            try:
+                draft = models.SubtitleDraft.objects.get(
+                    language=language, 
+                    parent_version=latest_version,
+                    user=request.user)
+            except:
+                pass
+        if not draft:
+            try:
+                draft = models.SubtitleDraft.objects.get(
+                    langauge=language,
+                    parent_version=latest_version,
+                    browser_id=request.browser_id)
+            except:
+                pass
+        return draft
 
     def _get_language_for_editing(self, request, video_id, language_code):
         video = models.Video.objects.get(video_id=video_id)
@@ -245,7 +242,7 @@ class Rpc(BaseRpc):
     def _subtitles_dict(self, version):
         language = version.language
         is_latest = False
-        latest_version = language.latest_finished_version()
+        latest_version = language.latest_version()
         if latest_version is None or version.version_no >= latest_version.version_no:
             is_latest = True
         return self._make_subtitles_dict(
@@ -256,7 +253,7 @@ class Rpc(BaseRpc):
             version.is_forked)
 
     def _subtitle_count(self, user, video):
-        version = video.latest_finished_version()
+        version = video.latest_version()
         return 0 if version is None else version.subtitle_set.count()
 
     def _initial_languages(self, user, video):
