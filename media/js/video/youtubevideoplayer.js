1cf037afb56bfd448397f257912981ad4bdeb60d
Adam Duston <aduston@gmail.com>
2011-01-20 17:24:25 -0600

    This fixes http://bugzilla.pculture.org/show_bug.cgi?id=15288

--- media/js/video/youtubevideoplayer.js
+++ media/js/video/youtubevideoplayer.js
@@ -96,7 +96,7 @@ mirosubs.video.YoutubeVideoPlayer.prototype.decorateInternal = function(element)
 };
 
 mirosubs.video.YoutubeVideoPlayer.prototype.createDom = function() {
-    mirosubs.video.YoutubeVideoPlayer.superClass_.createDom.call(this);
+    this.setElementInternal(this.getDomHelper().createElement('span'));
     var sizeFromConfig = this.sizeFromConfig_();
     if (!this.forDialog_ && sizeFromConfig)
         this.playerSize_ = sizeFromConfig;
@@ -110,9 +110,9 @@ mirosubs.video.YoutubeVideoPlayer.prototype.enterDocument = function() {
     mirosubs.video.YoutubeVideoPlayer.superClass_.enterDocument.call(this);
     if (!this.swfEmbedded_) {
         this.swfEmbedded_ = true;
-        var videoDiv = this.getDomHelper().createDom('div');
-        videoDiv.id = mirosubs.randomString();
-        this.getElement().appendChild(videoDiv);
+        var videoSpan = this.getDomHelper().createDom('span');
+        videoSpan.id = mirosubs.randomString();
+        this.getElement().appendChild(videoSpan);
         var params = { 'allowScriptAccess': 'always', 'wmode' : 'opaque' };
         var atts = { 'id': this.playerElemID_, 
                      'style': mirosubs.style.setSizeInString(
@@ -125,7 +125,7 @@ mirosubs.video.YoutubeVideoPlayer.prototype.enterDocument = function() {
                                this.videoSource_.getYoutubeVideoID());
         this.addQueryString_(uri);
         window["swfobject"]["embedSWF"](
-            uri.toString(), videoDiv.id, 
+            uri.toString(), videoSpan.id, 
             this.playerSize_.width + '', 
             this.playerSize_.height + '', 
             "8", null, null, params, atts);
