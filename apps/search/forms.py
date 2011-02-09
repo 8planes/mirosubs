db6da2c6d4cbd448e20ad7cbe5f379db7b19ca30
Alerion <alerion.um@gmail.com>
2011-02-09 18:25:56 +0200

    fix search, http://bugzilla.pculture.org/show_bug.cgi?id=16205

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -6,23 +6,20 @@ from utils.translation import get_languages_list
 ALL_LANGUAGES = tuple((val, _(name))for val, name in settings.ALL_LANGUAGES)
 
 class SearchForm(forms.Form):
-    TYPE_CHOICES = (
-        ('title', _(u'title')),
-        ('full_text', _(u'full-text'))
-    )
     SORT_TYPE_CHOICES = (
         ('asc', _(u'Ascending')),
         ('desc', _(u'Descending'))
     )
     SORT_CHOICES = (
-        ('date', _(u'Date')),
-        ('sub_fetch', _(u'Subtitles fetched')),
-        ('page_loads', _(u'Page loads')),
-        ('comments', _(u'Comments')),
+        ('', _('Most relevant')),
+        ('date', _(u'Newest')),
+        ('sub_fetch', _(u'Most subtitle plays')),
+        ('page_loads', _(u'Most page loads')),
+        ('comments', _(u'Most commented')),
         ('languages', _(u'Most languages')),
         ('edited', _(u'Last edited')),
-        ('contributors', _(u'Contributors')),
-        ('activity', _(u'Activity'))
+        ('contributors', _(u'Most commented')),
+        ('activity', _(u'Most activity'))
     )
     DISPLAY_CHOICES = (
         ('all', _(u'all')),
@@ -30,15 +27,11 @@ class SearchForm(forms.Form):
         #('details', _(u'details')),
     )
     q = forms.CharField(required=False, label=_(u'query'))
-    type = forms.ChoiceField(choices=TYPE_CHOICES, required=False, initial='full_text', 
-                             label=_(u'search type'))
-    st = forms.ChoiceField(choices=SORT_TYPE_CHOICES, required=False, initial='desc', 
-                           label=_(u'sort order'))
-    sort = forms.ChoiceField(choices=SORT_CHOICES, required=False, initial='sub_fetch', 
+    #st = forms.ChoiceField(choices=SORT_TYPE_CHOICES, required=False, initial='desc', 
+    #                       label=_(u'sort order'))
+    sort = forms.ChoiceField(choices=SORT_CHOICES, required=False, initial='', 
                              label=_(u'sort type'))
     langs = forms.ChoiceField(choices=ALL_LANGUAGES, required=False, label=_(u'languages'),
-                              help_text=_(u'Left blank for any language'))
-    excluded_langs = forms.ChoiceField(choices=ALL_LANGUAGES, required=False, label=_(u'excluded languages'),
                               help_text=_(u'Left blank for any language'))
     video_lang = forms.ChoiceField(choices=ALL_LANGUAGES, required=False, label=_(u'video language'),
                               help_text=_(u'Left blank for any language'))
@@ -51,21 +44,18 @@ class SearchForm(forms.Form):
         self.user_langs = user_langs
         choices[:0] = (
             ('my_langs', _(u'My languages')),
-            ('', _(u'Any')),
+            ('', _(u'Any Language')),
+            ('not_selected', 'Not ---'),
         )
         self.fields['langs'].choices = choices
         self.fields['video_lang'].choices = choices
-        self.fields['excluded_langs'].choices = get_languages_list(True)
         self.user = user
 
         
     def search_qs(self, qs):
         q = self.cleaned_data.get('q')
-        ordering = self.cleaned_data.get('sort') or 'page_loads'
-        order_type = self.cleaned_data.get('st') or 'asc'
+        ordering = self.cleaned_data.get('sort', '')
         langs = self.cleaned_data.get('langs')
-        type = self.cleaned_data.get('type') or 'full_text'
-        excluded_langs = self.cleaned_data.get('excluded_langs')
         video_language = self.cleaned_data.get('video_lang')
         
         order_fields = {
@@ -79,32 +69,31 @@ class SearchForm(forms.Form):
             'edited': 'edited'
         }
         
-        if q:
-            if type == 'full_text':
-                qs = qs.auto_query(q)
-            else:
-                qs = qs.filter(title=qs.query.clean(q))
+        qs = qs.auto_query(q)
         
         if video_language:
             if video_language == 'my_langs':
                 qs = qs.filter(video_language__in=self.user_langs)
+            elif video_language == 'not_selected' and langs not in ['', 'not_selected']:
+                if langs == 'my_langs':
+                    qs = qs.exclude(video_language__in=self.user_langs)
+                else:
+                    qs = qs.exclude(video_language=langs)
             else:
                 qs = qs.filter(video_language=video_language)
         
         if langs:
-            langs = [langs]
-            if 'my_langs' in langs:
-                del langs[langs.index('my_langs')]
-                for l in self.user_langs:
-                    if not l in langs:
-                        langs.append(l)
-
-            qs = qs.filter(languages__in=langs)
+            if langs == 'my_langs':
+                qs = qs.filter(languages__in=self.user_langs)
+            elif langs == 'not_selected' and video_language not in ['', 'not_selected']:
+                if video_language == 'my_langs':
+                    qs = qs.exclude(languages__in=self.user_langs)
+                else:
+                    qs = qs.exclude(languages=langs)
+            else:
+                qs = qs.filter(languages=langs)
         
-        if excluded_langs:
-            excluded_langs = [excluded_langs]
-            qs = qs.exclude(languages__in=excluded_langs)
-        
-        qs = qs.order_by(('-' if order_type == 'desc' else '')+order_fields[ordering])
+        if ordering:
+            qs = qs.order_by('-'+order_fields[ordering])
 
         return qs
