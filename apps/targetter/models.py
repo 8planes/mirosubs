7f466e128a8835b81d4fd7dc9958be66bc599556
Alerion <alerion.um@gmail.com>
2011-02-22 19:05:52 +0200

    update PCF Tragetter, https://www.pivotaltracker.com/story/show/9820397

--- apps/targetter/models.py
+++ apps/targetter/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from targetter import BROWSER_DICT, OS_DICT
+from django.utils.translation import ugettext_lazy as _
 
 class MessageConfig(models.Model):
     message = models.TextField()
     os = models.CommaSeparatedIntegerField(null=True, blank=True, max_length=200)
     browser = models.CommaSeparatedIntegerField(null=True, blank=True, max_length=200)
+    top_latitude = models.FloatField(blank=True, null=True)
+    top_longitude = models.FloatField(blank=True, null=True)
+    side_length = models.FloatField(blank=True, null=True, help_text=_(u'in degrees'))
     
     @models.permalink
     def get_absolute_url(self):
@@ -16,9 +20,13 @@ class MessageConfig(models.Model):
         }
         
         if self.os:
-            config['os'] = [OS_DICT.get(int(i), '') for i in self.os.split(',')]
+            config['os'] = [OS_DICT.get(int(i), '') for i in self.os.split(',') if i != '']
             
         if self.os:
-            config['userAgents'] = [BROWSER_DICT.get(int(i), '') for i in self.browser.split(',')]
-            
+            config['userAgents'] = [BROWSER_DICT.get(int(i), '') for i in self.browser.split(',') if i != '']
+        
+        config['top_latitude'] = self.top_latitude
+        config['top_longitude'] = self.top_longitude
+        config['side_length'] = self.side_length
+        
         return config            
