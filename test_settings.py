121f189a6194cc99a67100e5ae66a003efb655c8
Alerion <alerion.um@gmail.com>
2011-02-23 18:19:56 +0200

    fix test settings

--- test_settings.py
+++ test_settings.py
@@ -1,5 +1,5 @@
 from settings import *
-from dev_settings import *
+#from dev_settings import *
 
 ROOT_URLCONF = 'urls'
 
@@ -14,3 +14,13 @@ INSTALLED_APPS += ('django_nose', )
 INSTALLED_APPS = list(INSTALLED_APPS)
 INSTALLED_APPS.remove('mirosubs')
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+
+SITE_ID = 4
+SITE_NAME = 'mirosubs-dev'
+
+TWITTER_CONSUMER_KEY = '6lHYqtxzQBD3lQ55Chi6Zg'
+TWITTER_CONSUMER_SECRET = 'ApkJPIIbBKp3Wph0JBoAg2Nsk1Z5EG6PFTevNpd5Y00'
+
+VIMEO_API_KEY = 'e1a46f832f8dfa99652781ee0b39df12'
+VIMEO_API_SECRET = 'bdaeb531298eeee1'
