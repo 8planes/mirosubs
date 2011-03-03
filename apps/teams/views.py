2e7f9bea7551a37952150244d5e09169c492688b
Adam Duston <aduston@gmail.com>
2011-03-03 13:21:13 -0500

    Fixed team detail page native query

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -127,7 +127,7 @@ def detail(request, slug):
 
     qs = SubtitleLanguage.objects.filter(video__in=video_ids).filter(language__in=languages) \
         .exclude(writelock_time__gte=writelock_cutoff) \
-        .extra(where=['NOT ((SELECT vsl.is_complete FROM videos_subtitlelanguage AS vsl WHERE vsl.is_original = %s AND vsl.video_id = videos_subtitlelanguage.video_id) == %s AND videos_subtitlelanguage.is_original=%s)'], params=(True, False, False,)) \
+        .extra(where=['NOT ((SELECT vsl.is_complete FROM videos_subtitlelanguage AS vsl WHERE vsl.is_original = %s AND vsl.video_id = videos_subtitlelanguage.video_id) = %s AND videos_subtitlelanguage.is_original=%s)'], params=(True, False, False,)) \
         .distinct()
 
     # all videos not in the list
