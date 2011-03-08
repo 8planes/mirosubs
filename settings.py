7dd2aa66cbc6aa4917757e4d63f20c9c6994c7af
Alerion <alerion.um@gmail.com>
2011-03-08 14:16:41 +0200

    add sentry to apps

--- settings.py
+++ settings.py
@@ -353,6 +353,11 @@ INSTALLED_APPS = (
     'api',
     'targetter',
     'livesettings',
+    
+    'indexer',
+    'paging',
+    'sentry',
+    'sentry.client',    
     #'djcelery',
     'mirosubs' #dirty hack to fix http://code.djangoproject.com/ticket/5494 
 )
