90707efbff83c76d1e96b9420679c5ccbbc0d78e
Alerion <alerion.um@gmail.com>
2011-03-22 17:05:51 +0200

    fix tests

--- apps/localeurl/__init__.py
+++ apps/localeurl/__init__.py
@@ -1,14 +1,17 @@
 # Copyright (c) 2008 Joost Cassee
 # Licensed under the terms of the MIT License (see LICENSE.txt)
 
+django_reverse = None
+
 def patch_reverse():
+    global django_reverse
     from django.conf import settings
     from django.core import urlresolvers
     from django.utils import translation
     import localeurl.settings
     from localeurl import utils    
 
-    if localeurl.settings.URL_TYPE == 'path_prefix' and settings.USE_I18N:
+    if not django_reverse and localeurl.settings.URL_TYPE == 'path_prefix' and settings.USE_I18N:
         def reverse(viewname, urlconf=None, args=[], kwargs={}, prefix=None, current_app=None):
             kwargs = kwargs or {}
             locale = utils.supported_language(kwargs.pop('locale', translation.get_language()))
