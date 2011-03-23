eb8c8a876cb2729d4202ce7dffc8ca15c6f64b58
Rob Chekaluk <rchekaluk@pculture.org>
2011-03-23 18:25:30 -0400

    Manually updating master: move some settings to server_local_settings. Add Tracelyzer. Conditional ADMINS.

--- unisubs_settings.py
+++ unisubs_settings.py
@@ -25,49 +25,53 @@ from server_local_settings import *
 JS_USE_COMPILED = True
 
 DEBUG = False
-ADMINS = (
-  ('Adam Duston', 'adam@8planes.com'),
-  ('Holmes Wilson', 'hwilson@gmail.com'),
-  ('usubs-errors', 'usubs-errors@pculture.org')
-)
 
-HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
-SOLR_ROOT = "/usr/local/apache-solr-1.3.0/example"
 
 if INSTALLATION == DEV:
     SITE_ID = 13
     SITE_NAME = 'unisubsdev'
     FEEDBACK_EMAILS.append('aduston@gmail.com')
-    TWITTER_CONSUMER_KEY = 'Vbub5lMDc47d8cydMNCTQ'
-    VIMEO_API_KEY = 'e1a46f832f8dfa99652781ee0b39df12'
-    MEDIA_URL = 'http://dev.universalsubtitles.org/site_media/'
     REDIS_DB = "3"
     AWS_QUEUE_PREFIX = 'DEV'
-    CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
     EMAIL_SUBJECT_PREFIX = '[usubs-dev]'
+    ADMINS = (
+      ('Adam Duston', 'adam@8planes.com'),
+      ('Holmes Wilson', 'hwilson@gmail.com'),
+      ('usubs-errors', 'usubs-errors@pculture.org')
+    )
 elif INSTALLATION == STAGING:
     SITE_ID = 14
     SITE_NAME = 'unisubsstaging'
-    TWITTER_CONSUMER_KEY = 'XYDDYlf54CU9bKMfIbvgNg'
-    VIMEO_API_KEY = 'e1a46f832f8dfa99652781ee0b39df12'
-    MEDIA_URL = 'http://s3.staging.universalsubtitles.org/'
     REDIS_DB = "2"
     AWS_QUEUE_PREFIX = 'STAGING'
-    CACHE_BACKEND = 'memcached://ip-10-124-229-117.ec2.internal:61211/'
     SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-    # TODO: insert tracelyzer stuff here.
+    
+    # Tracelyzer instrumentation
+    # http://support.tracelytics.com/kb/instrumenting-your-app/instrumenting-django-apps
+    try:
+        from oboeware.django import install_oboe_instrumentation
+        MIDDLEWARE_CLASSES = install_oboe_instrumentation(MIDDLEWARE_CLASSES)
+    except ImportError:
+        import sys
+        print >> sys.stderr, "[oboe] Unable to instrument app and middleware"
+        pass # gracefully disable tracing if Tracelytics oboeware not present
+        
     EMAIL_SUBJECT_PREFIX = '[usubs-staging]'
+    ADMINS = (
+      ('Adam Duston', 'adam@8planes.com'),
+      ('Holmes Wilson', 'hwilson@gmail.com'),
+      ('usubs-errors', 'usubs-errors@pculture.org')
+    )
 elif INSTALLATION == PRODUCTION:
     SITE_ID = 8
     SITE_NAME = 'unisubs'
-    TWITTER_CONSUMER_KEY = '568zigeMu0p0KNHI3XUn0g'
-    VIMEO_API_KEY = 'e1a46f832f8dfa99652781ee0b39df12'
-    MEDIA_URL = 'http://s3.www.universalsubtitles.org/'
     REDIS_DB = "1"
     AWS_QUEUE_PREFIX = 'PRODUCTION'
-    CACHE_BACKEND = 'memcached://ip-10-124-229-117.ec2.internal:11211/'
     SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
     EMAIL_SUBJECT_PREFIX = '[usubs-production]'
+    ADMINS = (
+      ('Adam Duston', 'adam@8planes.com')
+    )
 
 IGNORE_REDIS = True
 
