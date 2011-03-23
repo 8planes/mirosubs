2112209de90b6878aa3981de48c2e31003aa4c39
Matt Johnson <mail@mattdetails.com>
2011-03-22 20:44:42 -0500

    updates

--- dev-settings.py
+++ dev-settings.py
@@ -31,7 +31,7 @@ MEDIA_URL = "http://{0}/site_media/".format(Site.objects.get(id=SITE_ID).domain)
 # MIDDLEWARE_CLASSES += ('middleware.SqlPrintingMiddleware',)
 
 # Uncomment following line when you want to work with compiled JS.
-#JS_USE_COMPILED = True
+JS_USE_COMPILED = True
 
 VIMEO_API_KEY = 'e1a46f832f8dfa99652781ee0b39df12'
 VIMEO_API_SECRET = 'bdaeb531298eeee1'
