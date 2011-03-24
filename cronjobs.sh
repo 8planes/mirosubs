3dad9ca2a352ae78d9ecf929a53996a37c6a0b75
Rob Chekaluk <rchekaluk@pculture.org>
2011-03-24 11:23:46 -0400

    Added a command line arg for application home directory.

--- cronjobs.sh
+++ cronjobs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-. /var/www/universalsubtitles.dev/env/bin/activate
-python /var/www/universalsubtitles.dev/mirosubs/manage.py update_from_feed --settings=unisubs-settings
-python /var/www/universalsubtitles.dev/mirosubs/manage.py send_notification --settings=unisubs-settings
+
+if [ -z "$1" ]; then
+   echo "ERROR: No application home directory provided"
+   exit 1
+fi
+
+apphome="$1"
+
+. $apphome/env/bin/activate
+python $apphome/mirosubs/manage.py update_from_feed --settings=unisubs-settings
+python $apphome/mirosubs/manage.py send_notification --settings=unisubs-settings
