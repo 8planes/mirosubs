7086ba48c476d69c1e3d5436ef17c69c3afbb9a6
Arthur Debert <arthur@stimuli.com.br>
2011-04-05 19:57:11 -0300

    Fixed bug where time_change or text_change could be > 100% (fixes story https://www.pivotaltracker.com/story/show/11934547)

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -837,8 +837,8 @@ class SubtitleVersion(SubtitleCollection):
                     text_count_changed += 1
                     time_count_changed += 1
                     
-            self.time_change = time_count_changed / 1. / subtitles_length
-            self.text_change = text_count_changed / 1. / subtitles_length
+            self.time_change = max(time_count_changed / 1. / subtitles_length, 1)
+            self.text_change = max(text_count_changed / 1. / subtitles_length, 1)
 
     def _get_standard_collection(self):
         if self.language.standard_language:
