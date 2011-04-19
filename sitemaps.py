4f05ccf0b2e37247052377a8ebe95c80f95b493f
Alerion <alerion.um@gmail.com>
2011-04-19 10:15:28 +0300

    reduce number of items in sitemap page

--- sitemaps.py
+++ sitemaps.py
@@ -88,7 +88,7 @@ class VideoSitemap(Sitemap):
     Definition of video pages, based on the videos available on site.
     TODO: Set video last modification time according to latest subtitle edition
     '''
-    limit = 10000
+    limit = 5000
     changefreq = "weekly"
     priority = 0.8
 
