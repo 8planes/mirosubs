1f32dc206c698fb28929be0c94cbf7b7de6fa102
Alerion <alerion.um@gmail.com>
2011-05-06 16:04:45 +0300

    fix statistic updating, https://www.pivotaltracker.com/story/show/12516579

--- utils/commands.py
+++ utils/commands.py
@@ -1,18 +1,10 @@
 from django.core.management.base import BaseCommand
-from django.core.mail import mail_admins
-import sys
+from sentry.client.models import client
 
 class ErrorHandlingCommand(BaseCommand):
     
-    def _handle_error(self, email_subtject, command_msg, exc_info):
-        message = "%s\n\n%s" % (self._get_traceback(exc_info),command_msg)
-        mail_admins(email_subtject, message, fail_silently=False)
-
-    def _get_traceback(self, exc_info=None):
-        import traceback
-        
-        "Helper function to return the traceback as a string"
-        return '\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))            
+    def handle_error(self, exc_info):
+        client.create_from_exception(exc_info)
     
     def print_to_console(self, msg, min_verbosity=1):
         if self.verbosity >= min_verbosity:
