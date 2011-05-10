15b5e97f1d230b8569632518f8cf4441fc77dc88
Matt Johnson <mail@mattdetails.com>
2011-05-10 15:33:57 -0500

    fixed issue with class.css messing up index, moved .note to global.css

--- settings.py
+++ settings.py
@@ -565,7 +565,6 @@ MEDIA_BUNDLES = {
             "css/mirosubs-widget.css",
             "css/top_user_panel.css",
             "css/buttons.css",
-            "css/classes.css", 
             "css/forms.css" ,
             "css/feedback.css", 
             "css/global.css" ,
