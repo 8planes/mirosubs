5d72bd4bc93db7eab6922cdc88b077491c9c6b15
Alerion <alerion.um@gmail.com>
2011-05-11 18:17:00 +0300

    remove User model from automatic updating, https://www.pivotaltracker.com/story/show/13259915

--- apps/auth/search_indexes.py
+++ apps/auth/search_indexes.py
@@ -3,7 +3,7 @@ from auth.models import CustomUser as User
 from haystack import site
 from utils.celery_search_index import CelerySearchIndex
 
-class UserIndex(CelerySearchIndex):
+class UserIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
     username = CharField()
     award_points = IntegerField(model_attr='award_points')
