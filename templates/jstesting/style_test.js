fca97ea09b1e92de55ae832cb76f2651131cd8aa
Adam Duston <aduston@gmail.com>
2011-05-12 18:54:04 -0400

    Fixed mirosubs.style unit tests, fixed mirosubs.style bug on ie. Fixes https://www.pivotaltracker.com/story/show/13326313

--- templates/jstesting/style_test.js
+++ templates/jstesting/style_test.js
@@ -7,49 +7,65 @@ function setUp() {
     testElem = document.createElement('div');
 }
 
+function makeStyleArray(value) {
+    return goog.array.filter(
+        goog.array.map(
+            value.split(';'),
+            function(s) { return s.toLowerCase(); }),
+        function(s) { return s.length > 0; });
+}
+
+function assertStyleEquals(expected, actual) {
+    var expectedSet = new goog.structs.Set(makeStyleArray(expected));
+    var actualArray = makeStyleArray(actual);
+    assertEquals(expectedSet.getCount(), actualArray.length);
+    for (var i = 0; i < actualArray.length; i++)
+        assertTrue(expectedSet.contains(actualArray[i]));
+}
+
 function testSetSize() {
     mirosubs.style.setSize(testElem, 32, 58);
-    assertEquals(testElem.style.cssText.replace(/ /gi, ''),
-                 "width:32px!important;height:58px!important;");
+    assertStyleEquals(
+        "width:32px!important;height:58px!important;",
+        testElem.style.cssText.replace(/ /gi, ''));
     mirosubs.style.setWidth(testElem, 33);
-    assertEquals(testElem.style.cssText.replace(/ /gi, ''),
-                 "width:33px!important;height:58px!important;");
+    assertStyleEquals("width:33px!important;height:58px!important;",
+                 testElem.style.cssText.replace(/ /gi, ''));
     mirosubs.style.setHeight(testElem, 59);
-    assertEquals(testElem.style.cssText.replace(/ /gi, ''),
-                 "width:33px!important;height:59px!important;");
+    assertStyleEquals("width:33px!important;height:59px!important;",
+                 testElem.style.cssText.replace(/ /gi, ''));
 }
 
 function testSetPosition() {
     mirosubs.style.setPosition(testElem, new goog.math.Coordinate(32, 58));
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 "left:32px!important;top:58px!important;");
+    assertStyleEquals("left:32px!important;top:58px!important;",
+                 testElem.style.cssText.replace(/ /g, ''));
     mirosubs.style.setPosition(testElem, null, 59);
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 "left:32px!important;top:59px!important;");
+    assertStyleEquals("left:32px!important;top:59px!important;",
+                 testElem.style.cssText.replace(/ /g, ''));
     mirosubs.style.setPosition(testElem, 33, null);
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 "left:33px!important;top:59px!important;");
+    assertStyleEquals("left:33px!important;top:59px!important;",
+                 testElem.style.cssText.replace(/ /g, ''));
     mirosubs.style.setPosition(testElem, new goog.math.Coordinate(34, 60));
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 "left:34px!important;top:60px!important;");
+    assertStyleEquals("left:34px!important;top:60px!important;",
+                 testElem.style.cssText.replace(/ /g, ''));
 }
 
 function testShow() {
     mirosubs.style.showElement(testElem, false);
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 'display:none!important;');
+    assertStyleEquals('display:none!important;',
+                 testElem.style.cssText.replace(/ /g, ''));
     mirosubs.style.showElement(testElem, true);
     assertEquals('', testElem.style.cssText);
 }
 
 function testSetVisibility() {
     mirosubs.style.setVisibility(testElem, false);
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 'visibility:hidden!important;');
+    assertStyleEquals('visibility:hidden!important;',
+                 testElem.style.cssText.replace(/ /g, ''));
     mirosubs.style.setVisibility(testElem, true);
-    assertEquals(testElem.style.cssText.replace(/ /g, ''),
-                 'visibility:visible!important;');
-
+    assertStyleEquals('visibility:visible!important;',
+                 testElem.style.cssText.replace(/ /g, ''));
 }
 
 {% endblock %}
