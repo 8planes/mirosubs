8dca85404ef3a80d1bc4d5e2e3edd58af8b2de48
alerion <alerion.um@gmail.com>
2011-05-26 17:47:00 +0300

    fix bug with opened DB connection in celeryd process, https://www.pivotaltracker.com/story/show/13694277

--- apps/messages/tasks.py
+++ apps/messages/tasks.py
@@ -2,17 +2,21 @@ from celery.decorators import task
 from utils import send_templated_email
 from messages.models import Message
 from django.contrib.sites.models import Site
+from sentry.client.models import client
+from django.db import connection
 
 @task()
 def send_new_message_notification(message_id):
     try:
         message = Message.objects.get(pk=message_id)
     except Message.DoesNotExist:
+        msg = '**send_new_message_notification**. Message does not exist. ID: %s' % message_id
+        client.create_from_text(msg, data=dict(), logger='celery')
         return
-    
+
     if not message.user.email or not message.user.is_active:
         return
-    
+
     to = "%s <%s>" % (message.user, message.user.email)
     subject = u"%s sent you a message on Universal Subtitles: %s" % (message.author, message.subject)
     context = {
@@ -20,6 +24,6 @@ def send_new_message_notification(message_id):
         "body": message.content,
         "domain":  Site.objects.get_current().domain,
         "message_pk": message.pk
-        }
+    }
 
     send_templated_email(to, subject, "messages/email/message_received.html", context)
--- apps/statistic/log_methods.py
+++ apps/statistic/log_methods.py
@@ -5,6 +5,7 @@ from django.contrib.sites.models import Site
 from django.contrib.admin.options import ModelAdmin
 from django.db import models
 from django.views.generic.simple import direct_to_template
+from django import db
 
 """
 Admin interface
@@ -79,6 +80,13 @@ class RedisLogBackend(BaseLogBackend):
         self.conn = connection
         self.prefix = prefix
         self.site_name = Site.objects.get_current().name.replace(':', '')
+        
+        #Site.objects.get_current() created new DB connection
+        #RedisLogBackend.__init__ is executed when you add it to class
+        #so, for example, celery worker process will have opened DB connection
+        #that cause bugs
+        db.connection.close()
+        
         #keys cache
         self.keys = {}
         self.key_set = self._get_key('logger_keys')
