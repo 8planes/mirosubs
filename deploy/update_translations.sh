8d8ed5fd03401c231428eead97668b2c947b572e
Adam Duston <aduston@gmail.com>
2011-06-15 16:26:50 -0400

    Corrected mistake in update_translations.sh

--- deploy/update_translations.sh
+++ deploy/update_translations.sh
@@ -6,10 +6,11 @@ python manage.py makemessages -d djangojs -i deploy\* -i media\js\closure-librar
 echo "pushing to transifex" && \
 tx push --source && \
 echo "pulling from transiflex" && \
-tx pull &&  echo "compiling messages" && \
+tx pull &&  \
+echo "compiling messages" && \
 python manage.py compilemessages && \
 echo "adding to git" && \
-git add /locale/*/LC_MESSAGES/django.*o && \
+git add locale/*/LC_MESSAGES/django.*o && \
 echo "committing to rep" && \
 git commit -m "Updated transiflex translations -- through update_translations.sh" && \
 echo "pushing to rep" && \
