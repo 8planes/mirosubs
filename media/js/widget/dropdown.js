23d63935e2b03c50a65aab036e3c7b9dd5cc92e0
Adam Duston <aduston@gmail.com>
2011-06-21 11:49:45 -0400

    Now hides Improve Subtitles link in widget dropdown when no subs are selected. Fixes https://www.pivotaltracker.com/story/show/14812261

--- media/js/widget/dropdown.js
+++ media/js/widget/dropdown.js
@@ -69,14 +69,7 @@ mirosubs.widget.DropDown.prototype.setCurrentSubtitleState = function(subtitleSt
     this.clearCurrentLang_();
     this.subtitleState_ = subtitleState;
     this.setCurrentLangClassName_();
-    // there is likely a bug in mirosubs.styleSetPropertyInString,
-    // turning it on and off will not restore display to block
-    // mirosubs.style.showElement(this.improveSubtitlesLink_, shows);
-    var display = "none";
-    if (Boolean(subtitleState)){
-        display = "block";
-    }
-    goog.style.setStyle(this.improveSubtitlesLink_, display + " !important");
+    mirosubs.style.showElement(this.improveSubtitlesLink_, !!subtitleState);
     goog.dom.getFirstElementChild(this.downloadSubtitlesLink_).href = this.createDownloadSRTURL_();
 };
 
