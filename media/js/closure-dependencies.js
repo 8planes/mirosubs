ea95073572f92772ecaa6796b2b8c8e56336a9ee
Adam Duston <aduston@gmail.com>
2011-07-15 18:36:52 -0400

    Removed CrossDomainRPC

--- media/js/closure-dependencies.js
+++ media/js/closure-dependencies.js
@@ -19,7 +19,6 @@
 goog.require("goog.ui.Dialog");
 goog.require("goog.net.Cookies");
 goog.require("goog.json.Serializer");
-goog.require("goog.net.CrossDomainRpc");
 goog.require("goog.dom.DomHelper");
 goog.require("goog.events.BrowserEvent");
 goog.require("goog.ui.Button");
