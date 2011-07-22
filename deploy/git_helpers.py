b4e82f596176edbe35d50abda0fcb16a940685eb
Adam Duston <aduston@gmail.com>
2011-07-22 10:44:32 -0500

    temporary workaround for failed git process interaction on startup

--- deploy/git_helpers.py
+++ deploy/git_helpers.py
@@ -5,7 +5,6 @@ import re
 def get_current_commit_hash(short=True):
     process = Popen(["git", "rev-parse", "HEAD"], stdout=PIPE)
     guid =  process.communicate()[0].strip()
-    print("git rev-parse HEAD returned {0}".format(guid))
     if guid:
         guid = guid[:8]
     return guid
@@ -13,7 +12,6 @@ def get_current_commit_hash(short=True):
 def get_current_branch():
     process = Popen(["git", "branch"], stdout=PIPE)
     branches = process.communicate()[0].strip()
-    print("git branch returned {0}".format(branches))
     branch = re.search(r"\* ([^\n]+)", branches).group(1)
     return branch
 
