c82c9944cdca1953c020780fbb5b5616540b4f7a
Adam Duston <aduston@gmail.com>
2011-07-22 10:31:37 -0500

    Added some diagnostics to git_helpers

--- deploy/git_helpers.py
+++ deploy/git_helpers.py
@@ -5,6 +5,7 @@ import re
 def get_current_commit_hash(short=True):
     process = Popen(["git", "rev-parse", "HEAD"], stdout=PIPE)
     guid =  process.communicate()[0].strip()
+    print("git rev-parse HEAD returned {0}".format(guid))
     if guid:
         guid = guid[:8]
     return guid
@@ -12,6 +13,7 @@ def get_current_commit_hash(short=True):
 def get_current_branch():
     process = Popen(["git", "branch"], stdout=PIPE)
     branches = process.communicate()[0].strip()
+    print("git branch returned {0}".format(branches))
     branch = re.search(r"\* ([^\n]+)", branches).group(1)
     return branch
 
