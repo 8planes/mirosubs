8planes/mirosubs

commit 303a3e4341f29a37cfa6988024d77f45c7d49e3e

Extended media compilation to js bundles

settings.py
@@ -166,149 +166,149 @@ USER_LANGUAGES_COOKIE_NAME = 'unisub-languages-cookie'
 
 # paths provided relative to media/js
 JS_CORE = \
-    ['mirosubs.js', 
-     'rpc.js',
-     'clippy.js',
-     'flash.js',
-     'spinner.js',
-     'sliderbase.js',
-     'closingwindow.js',
-     'loadingdom.js',
-     'tracker.js',
-     'style.js',
-     'messaging/simplemessage.js',
-     'video/video.js',
-     'video/captionview.js',
-     'video/abstractvideoplayer.js',
-     'video/flashvideoplayer.js',
-     'video/html5videoplayer.js',
-     'video/youtubevideoplayer.js',
-     'video/jwvideoplayer.js',
-     'video/flvvideoplayer.js',
-     'video/videosource.js',
-     'video/html5videosource.js',
-     'video/youtubevideosource.js',
-     'video/brightcovevideosource.js',
-     'video/brightcovevideoplayer.js',
-     'video/flvvideosource.js',
-     'video/bliptvplaceholder.js',
-     'video/controlledvideoplayer.js',
-     'video/vimeovideosource.js',
-     'video/vimeovideoplayer.js',
-     'video/dailymotionvideosource.js',
-     'video/dailymotionvideoplayer.js',
-     'startdialog/model.js',
-     'startdialog/videolanguage.js',
-     'startdialog/videolanguages.js',
-     'startdialog/tolanguage.js',
-     'startdialog/tolanguages.js',
-     'startdialog/dialog.js',
-     'requestdialog.js',
-     'widget/subtitle/editablecaption.js',
-     "widget/subtitle/editablecaptionset.js",
-     'widget/usersettings.js',
-     'widget/logindialog.js',
-     'widget/videotab.js',
-     'widget/howtovideopanel.js',
-     'widget/dialog.js',
-     'widget/captionmanager.js',
-     'widget/rightpanel.js',
-     'widget/basestate.js',
-     'widget/subtitlestate.js',
-     'widget/dropdowncontents.js',
-     'widget/playcontroller.js',
-     'widget/subtitlecontroller.js',
-     'widget/subtitledialogopener.js',
-     'widget/opendialogargs.js',
-     'widget/dropdown.js',
-     'widget/play/manager.js',
-     'widget/widgetcontroller.js',
-     'widget/widget.js'
+    ['js/mirosubs.js', 
+     'js/rpc.js',
+     'js/clippy.js',
+     'js/flash.js',
+     'js/spinner.js',
+     'js/sliderbase.js',
+     'js/closingwindow.js',
+     'js/loadingdom.js',
+     'js/tracker.js',
+     'js/style.js',
+     'js/messaging/simplemessage.js',
+     'js/video/video.js',
+     'js/video/captionview.js',
+     'js/video/abstractvideoplayer.js',
+     'js/video/flashvideoplayer.js',
+     'js/video/html5videoplayer.js',
+     'js/video/youtubevideoplayer.js',
+     'js/video/jwvideoplayer.js',
+     'js/video/flvvideoplayer.js',
+     'js/video/videosource.js',
+     'js/video/html5videosource.js',
+     'js/video/youtubevideosource.js',
+     'js/video/brightcovevideosource.js',
+     'js/video/brightcovevideoplayer.js',
+     'js/video/flvvideosource.js',
+     'js/video/bliptvplaceholder.js',
+     'js/video/controlledvideoplayer.js',
+     'js/video/vimeovideosource.js',
+     'js/video/vimeovideoplayer.js',
+     'js/video/dailymotionvideosource.js',
+     'js/video/dailymotionvideoplayer.js',
+     'js/startdialog/model.js',
+     'js/startdialog/videolanguage.js',
+     'js/startdialog/videolanguages.js',
+     'js/startdialog/tolanguage.js',
+     'js/startdialog/tolanguages.js',
+     'js/startdialog/dialog.js',
+     'js/requestdialog.js',
+     'js/widget/subtitle/editablecaption.js',
+     "js/widget/subtitle/editablecaptionset.js",
+     'js/widget/usersettings.js',
+     'js/widget/logindialog.js',
+     'js/widget/videotab.js',
+     'js/widget/howtovideopanel.js',
+     'js/widget/dialog.js',
+     'js/widget/captionmanager.js',
+     'js/widget/rightpanel.js',
+     'js/widget/basestate.js',
+     'js/widget/subtitlestate.js',
+     'js/widget/dropdowncontents.js',
+     'js/widget/playcontroller.js',
+     'js/widget/subtitlecontroller.js',
+     'js/widget/subtitledialogopener.js',
+     'js/widget/opendialogargs.js',
+     'js/widget/dropdown.js',
+     'js/widget/play/manager.js',
+     'js/widget/widgetcontroller.js',
+     'js/widget/widget.js'
 ]
 
 JS_DIALOG = \
-    ['subtracker.js',
-     'srtwriter.js',
-     'widget/unsavedwarning.js',
-     'widget/resumeeditingrecord.js',
-     'widget/droplockdialog.js',
-     'finishfaildialog/dialog.js',
-     'finishfaildialog/errorpanel.js',
-     'finishfaildialog/reattemptuploadpanel.js',
-     'finishfaildialog/copydialog.js',
-     'widget/subtitle/dialog.js',
-     'widget/subtitle/msservermodel.js',
-     'widget/subtitle/subtitlewidget.js',
-     'widget/subtitle/addsubtitlewidget.js',
-     'widget/subtitle/subtitlelist.js',
-     'widget/subtitle/transcribeentry.js',
-     'widget/subtitle/transcribepanel.js',
-     'widget/subtitle/transcriberightpanel.js',
-     'widget/subtitle/syncpanel.js',
-     'widget/subtitle/reviewpanel.js',
-     'widget/subtitle/reviewrightpanel.js',
-     'widget/subtitle/sharepanel.js',
-     'widget/subtitle/completeddialog.js',
-     'widget/subtitle/editpanel.js',
-     'widget/subtitle/editrightpanel.js',
-     'widget/subtitle/bottomfinishedpanel.js',
-     'widget/subtitle/logger.js',
-     'widget/subtitle/savedsubtitles.js',
-     'widget/timeline/timerow.js',
-     'widget/timeline/timerowul.js',
-     'widget/timeline/timelinesub.js',
-     'widget/timeline/timelinesubs.js',
-     'widget/timeline/timelineinner.js',
-     'widget/timeline/timeline.js',
-     'widget/timeline/subtitle.js',
-     'widget/timeline/subtitleset.js',
-     'widget/controls/bufferedbar.js',
-     'widget/controls/playpause.js',
-     'widget/controls/progressbar.js',
-     'widget/controls/progressslider.js',
-     'widget/controls/timespan.js',
-     'widget/controls/videocontrols.js',
-     'widget/controls/volumecontrol.js',
-     'widget/controls/volumeslider.js',
-     'widget/translate/googletranslator.js',
-     'widget/translate/dialog.js',
-     'widget/translate/translationpanel.js',
-     'widget/translate/translationlist.js',
-     'widget/translate/translationwidget.js',
-     'widget/translate/translationrightpanel.js',
-     'widget/translate/forkdialog.js',
-     'widget/translate/titletranslationwidget.js']
+    ['js/subtracker.js',
+     'js/srtwriter.js',
+     'js/widget/unsavedwarning.js',
+     'js/widget/resumeeditingrecord.js',
+     'js/widget/droplockdialog.js',
+     'js/finishfaildialog/dialog.js',
+     'js/finishfaildialog/errorpanel.js',
+     'js/finishfaildialog/reattemptuploadpanel.js',
+     'js/finishfaildialog/copydialog.js',
+     'js/widget/subtitle/dialog.js',
+     'js/widget/subtitle/msservermodel.js',
+     'js/widget/subtitle/subtitlewidget.js',
+     'js/widget/subtitle/addsubtitlewidget.js',
+     'js/widget/subtitle/subtitlelist.js',
+     'js/widget/subtitle/transcribeentry.js',
+     'js/widget/subtitle/transcribepanel.js',
+     'js/widget/subtitle/transcriberightpanel.js',
+     'js/widget/subtitle/syncpanel.js',
+     'js/widget/subtitle/reviewpanel.js',
+     'js/widget/subtitle/reviewrightpanel.js',
+     'js/widget/subtitle/sharepanel.js',
+     'js/widget/subtitle/completeddialog.js',
+     'js/widget/subtitle/editpanel.js',
+     'js/widget/subtitle/editrightpanel.js',
+     'js/widget/subtitle/bottomfinishedpanel.js',
+     'js/widget/subtitle/logger.js',
+     'js/widget/subtitle/savedsubtitles.js',
+     'js/widget/timeline/timerow.js',
+     'js/widget/timeline/timerowul.js',
+     'js/widget/timeline/timelinesub.js',
+     'js/widget/timeline/timelinesubs.js',
+     'js/widget/timeline/timelineinner.js',
+     'js/widget/timeline/timeline.js',
+     'js/widget/timeline/subtitle.js',
+     'js/widget/timeline/subtitleset.js',
+     'js/widget/controls/bufferedbar.js',
+     'js/widget/controls/playpause.js',
+     'js/widget/controls/progressbar.js',
+     'js/widget/controls/progressslider.js',
+     'js/widget/controls/timespan.js',
+     'js/widget/controls/videocontrols.js',
+     'js/widget/controls/volumecontrol.js',
+     'js/widget/controls/volumeslider.js',
+     'js/widget/translate/googletranslator.js',
+     'js/widget/translate/dialog.js',
+     'js/widget/translate/translationpanel.js',
+     'js/widget/translate/translationlist.js',
+     'js/widget/translate/translationwidget.js',
+     'js/widget/translate/translationrightpanel.js',
+     'js/widget/translate/forkdialog.js',
+     'js/widget/translate/titletranslationwidget.js']
 
 JS_OFFSITE = list(JS_CORE)
-JS_OFFSITE.append('widget/crossdomainembed.js')
+JS_OFFSITE.append('js/widget/crossdomainembed.js')
 
 JS_ONSITE = list(JS_CORE)
 JS_ONSITE.extend(
-    ['srtwriter.js',
-     'widget/samedomainembed.js',
-     "widget/api/servermodel.js",
-     "widget/api/api.js"])
+    ['js/srtwriter.js',
+     'js/widget/samedomainembed.js',
+     "js/widget/api/servermodel.js",
+     "js/widget/api/api.js"])
 
 JS_WIDGETIZER_CORE = list(JS_CORE)
 JS_WIDGETIZER_CORE.extend([
-    "widget/widgetdecorator.js",
-    "widgetizer/videoplayermaker.js",
-    "widgetizer/widgetizer.js",
-    "widgetizer/youtube.js",
-    "widgetizer/html5.js",
-    "widgetizer/jwplayer.js"])
+    "js/widget/widgetdecorator.js",
+    "js/widgetizer/videoplayermaker.js",
+    "js/widgetizer/widgetizer.js",
+    "js/widgetizer/youtube.js",
+    "js/widgetizer/html5.js",
+    "js/widgetizer/jwplayer.js"])
 
 JS_WIDGETIZER = list(JS_WIDGETIZER_CORE)
-JS_WIDGETIZER.append('widgetizer/dowidgetize.js')
+JS_WIDGETIZER.append('js/widgetizer/dowidgetize.js')
 
 JS_EXTENSION = list(JS_WIDGETIZER_CORE)
-JS_EXTENSION.append('widgetizer/extension.js')
+JS_EXTENSION.append('js/widgetizer/extension.js')
 
 JS_API = list(JS_CORE)
 JS_API.extend(JS_DIALOG)
 JS_API.extend([
-        "widget/api/servermodel.js",
-        "widget/api/api.js"])
+        "js/widget/api/servermodel.js",
+        "js/widget/api/api.js"])
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -520,7 +520,7 @@ try:
     from commit import LAST_COMMIT_GUID
 except ImportError:
     print "deploy/create_commit_file must be ran before boostrapping django"
-    raise
+    LAST_COMMIT_GUID = "dev/dev"
 
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
@@ -609,32 +609,53 @@ MEDIA_BUNDLES = {
      },
      "mirosubs-widgetizer":{
         "type": "js",
-        "files": ["config.js"] + JS_WIDGETIZER,
+        "closure_deps": "js/closure-dependencies.js",
+        "files": ["js/config.js"] + JS_WIDGETIZER,
      },
     "mirosubs-widgetizer-debug":{
         "type": "js",
-        "files": ["config.js" ] + JS_WIDGETIZER,
+        "files": ["js/config.js" ] + JS_WIDGETIZER,
+        "closure_deps": "js/closure-dependencies.js",
         "debug": True,
      },
     "mirosubs-extension":{
         "type": "js",
-        "files": ["config.js" ] + JS_EXTENSION,
+        "files": ["js/config.js" ] + JS_EXTENSION,
      },
 
     "mirosubs-statwidget":{
         "type": "js",
-        "closure_deps": "closure-stat-dependencies.js",
+        "closure_deps": "js/closure-stat-dependencies.js",
         "include_flash_deps": False,
         "files": [
-            'mirosubs.js',
-            'rpc.js',
-            'loadingdom.js',
-            'statwidget/statwidgetconfig.js',
-            'statwidget/statwidget.js'],
+            'js/mirosubs.js',
+            'js/rpc.js',
+            'js/loadingdom.js',
+            'js/statwidget/statwidgetconfig.js',
+            'js/statwidget/statwidget.js'],
      },
 
     "mirosubs-api":{
         "type": "js",
-        "files": ["config.js"] + JS_API,
+        "files": ["js/config.js"] + JS_API,
      },
+    "site_base_js":{
+        "type":"js",
+        "files":[
+              "js/jquery-1.4.3.js",
+              "js/jquery-ui-1.8.13.custom.min.js",
+              "js/jgrowl/jquery.jgrowl.js",
+              "js/jalerts/jquery.alerts.js",
+              "js/jquery.form.js",
+              "js/jquery.metadata.js",
+              "js/jquery.mod.js",
+              "js/jquery.rpc.js",
+              "js/jquery.input_replacement.min.js",
+              "js/messages.js",
+
+            ],
+        "optimizations": "SIMPLE_OPTIMIZATIONS",
+        "closure_deps": "",
+        "include_flash_deps":False,
+        }
 }

apps/unisubs_compressor/management/commands/compile_media.py
@@ -31,8 +31,8 @@ logging.basicConfig(level=logging.INFO,
 MEDIA_ROOT = settings.MEDIA_ROOT
 def to_media_root(*paths):
     return os.path.join(settings.MEDIA_ROOT, *paths)
-JS_LIB = os.path.join(settings.PROJECT_ROOT, "media/js")
-CLOSURE_LIB = os.path.join(JS_LIB, "closure-library")
+JS_LIB = os.path.join(settings.PROJECT_ROOT, "media")
+CLOSURE_LIB = os.path.join(JS_LIB, "js", "closure-library")
 FLOWPLAYER_JS = os.path.join(settings.PROJECT_ROOT, "media/flowplayer/flowplayer-3.2.2.min.js")
 COMPILER_PATH = os.path.join(settings.PROJECT_ROOT,  "closure", "compiler.jar")
 
@@ -57,7 +57,7 @@ def call_command(command):
     return process.communicate()
 
 def get_cache_dir():
-    commit_hash = get_current_commit_hash()
+    commit_hash = settings.LAST_COMMIT_GUID.split("/")[1]
     return os.path.join(settings.MEDIA_ROOT, settings.COMPRESS_OUTPUT_DIRNAME, commit_hash)
 
 def get_cache_base_url():
@@ -106,12 +106,13 @@ class Command(BaseCommand):
         bundle_settings = settings.MEDIA_BUNDLES[bundle_name]
         debug = bundle_settings.get("debug", False)
         include_flash_deps = bundle_settings.get("include_flash_deps", True)
-        closure_dep_file = bundle_settings.get("closure_deps",'closure-dependencies.js' )        
+        closure_dep_file = bundle_settings.get("closure_deps",'js/closure-dependencies.js' )
+        optimization_type = bundle_settings.get("optimizations", "ADVANCED_OPTIMIZATIONS")
 
         logging.info("Starting {0}".format(output_file_name))
 
         deps = [" --js %s " % os.path.join(JS_LIB, file) for file in files]
-        calcdeps_js = os.path.join(JS_LIB, 'mirosubs-calcdeps.js')
+        calcdeps_js = os.path.join(JS_LIB, 'js', 'mirosubs-calcdeps.js')
         compiled_js = os.path.join(settings.MEDIA_ROOT, "js" , output_file_name)
         compiler_jar = COMPILER_PATH
 
@@ -119,7 +120,7 @@ class Command(BaseCommand):
 
         js_debug_dep_file = ''
         if debug:
-            js_debug_dep_file = '-i {0}/{1}'.format(JS_LIB, 'closure-debug-dependencies.js')
+            js_debug_dep_file = '-i {0}/{1}'.format(JS_LIB, 'js/closure-debug-dependencies.js')
 
         output,_ = call_command(("%s/closure/bin/calcdeps.py -i %s/%s %s " +
                                  "-p %s/ -o script") % 
@@ -148,16 +149,16 @@ class Command(BaseCommand):
                                    "%s "
                                    "--define goog.NATIVE_ARRAY_PROTOTYPES=false "
                                    "--output_wrapper (function(){%%output%%})(); "
-                                   "--compilation_level ADVANCED_OPTIMIZATIONS") % 
+                                   "--compilation_level %s") % 
                                   (compiler_jar, calcdeps_js, deps, compiled_js,
-                                   debug_arg))
+                                   debug_arg, optimization_type))
 
         with open(compiled_js, 'r') as compiled_js_file:
             compiled_js_text = compiled_js_file.read()
 
         with open(compiled_js, 'w') as compiled_js_file:
             if include_flash_deps:
-                with open(os.path.join(JS_LIB, 'swfobject.js'), 'r') as swfobject_file:
+                with open(os.path.join(JS_LIB, 'js', 'swfobject.js'), 'r') as swfobject_file:
                     compiled_js_file.write(swfobject_file.read())
                 with open(FLOWPLAYER_JS, 'r') as flowplayerjs_file:
                     compiled_js_file.write(flowplayerjs_file.read())

apps/unisubs_compressor/templatetags/media_compressor.py
@@ -26,20 +26,31 @@ logger = logging.getLogger(__name__)
 
 should_compress = None
 
-@register.inclusion_tag("uni_compressor/css_links.html")
-def include_css_bundle(bundle_name):
+@register.simple_tag
+def include_bundle(bundle_name):
     global should_compress
     if should_compress is None:
-        should_compress = getattr(settings, "CSS_USE_COMPILED",
+        should_compress = getattr(settings, "COMPRESS_MEDIA",
                                   not getattr(settings, "DEBUG", False))
+    bundle_type = settings.MEDIA_BUNDLES.get(bundle_name)["type"]
+
     if  should_compress == True:
-        urls = ["css-compressed/%s.css" % ( bundle_name)]
+        base = ""
+        if bundle_type == "css":
+            base =  "css-compressed/"
+        elif bundle_type == "js":
+            base = "js/"
+        
+        urls = ["%s%s.%s" % ( base, bundle_name, bundle_type)]
     else:
         urls = settings.MEDIA_BUNDLES.get(bundle_name)["files"]
+        
         if should_compress:
             logger.warning("could not find final url for %s" % bundle_name)
-    return {
+
+    return template.loader.render_to_string("uni_compressor/%s_links.html" % bundle_type,{
         "urls":urls,
-        "MEDIA_URL": settings.MEDIA_URL
-    }    
+        "MEDIA_URL": settings.MEDIA_URL,
+        "bundle_type": bundle_type,
+    })
                            

deploy/fabfile.py
@@ -405,5 +405,8 @@ def test_memcached():
             if output.find(random_string) == -1:
                 raise Exception('Machines {0} and {1} are using different memcached instances'.format(
                         host, other_host))
-                
 
+def generate_docs():
+    env.host_string = DEV_HOST
+    with cd(os.path.join(env.static_dir, 'mirosubs')):
+         sphinx-build docs/ media/docs

dev_settings.py
@@ -54,7 +54,7 @@ except ImportError:
 if USE_AMAZON_S3:
     AWS_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
 
-USE_BUNDLED_MEDIA = not DEBUG
-if USE_BUNDLED_MEDIA:
+COMPRESS_MEDIA = not DEBUG
+if COMPRESS_MEDIA:
     MEDIA_URL += "%s/%s/" % (COMPRESS_OUTPUT_DIRNAME, LAST_COMMIT_GUID.split("/")[1])
 

templates/base.html
@@ -14,7 +14,7 @@
 {% block css %}
 
 
-{% include_css_bundle "base" %}
+{% include_bundle "base" %}
 
 <!--[if gte IE 8]>
 <link href="{{ MEDIA_URL }}css/ie8.css" media="all" type="text/css" rel="stylesheet" />
@@ -27,22 +27,11 @@
 {% endblock %}
 <link rel="shortcut icon" type="image/png" href="{{ MEDIA_URL }}images/favicon.png" />
 <link rel="icon" type="image/png" href="{{ MEDIA_URL }}images/favicon.png" />
-{% if DEBUG %}
-<script src="{{ MEDIA_URL }}js/jquery-1.4.3.js" type="text/javascript"></script>
-{% else %}
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
-{% endif %}
-<script src="{{ MEDIA_URL }}js/jquery-ui-1.8.13.custom.min.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jgrowl/jquery.jgrowl.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jalerts/jquery.alerts.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jquery.form.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jquery.metadata.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jquery.mod.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jquery.rpc.js" type="text/javascript"></script>
-<script src="{{ MEDIA_URL }}js/jquery.input_replacement.min.js" type="text/javascript"></script>
+{% include_bundle "site_base_js" %}
+
+
 <script type="text/javascript" src="{% url js_i18n_catalog %}"></script>
 <script src="{% url profiles:rpc_api %}" type="text/javascript"></script>  
-<script src="{{ MEDIA_URL }}js/messages.js" type="text/javascript"></script>
 
 {% block scripts %}{% endblock %}
 <title>

templates/index.html
@@ -4,8 +4,8 @@
 
 {% block css %}
 
-{% include_css_bundle "base" %}
-    {% include_css_bundle "home" %}
+{% include_bundle "base" %}
+{% include_bundle "home" %}
 
 {% endblock %}
 

templates/pagedemo/boingboing_embed.html
@@ -29,11 +29,7 @@
     <script type="text/javascript" src="{{MEDIA_URL}}js/mirosubs-widgetizer.js"></script>
 {% endifequal %}
 
-{% ifequal type "embed" %}
-     {% if not js_use_compiled %}
-         {% include "widget/_js_base_dependencies.html" %}
-      {% endif %}
-{% endifequal %}
+
 </head>
 
 

commit 979ce6813486017a74f088469c9001d55f1ee044

Fixes breakage caused by https://github.com/8planes/mirosubs/commit/303a3e4341f29a37cfa6988024d77f45c7d49e3e#diff-4 . Should fix https://www.pivotaltracker.com/story/show/16250081

apps/widget/__init__.py
@@ -25,7 +25,7 @@ import simplejson as json
 LANGUAGES_MAP = dict(LANGUAGES)
 
 def full_path(js_file):
-    return "{0}js/{1}".format(settings.MEDIA_URL, js_file)
+    return "{0}{1}".format(settings.MEDIA_URL, js_file)
 
 def add_offsite_js_files(context):
     """ Adds variables necessary for _js_dependencies.html """

media/js/mirosubs.js
@@ -348,11 +348,16 @@ mirosubs.isReturnURLInDifferentDomain = function() {
         return false;
     var uri = new goog.Uri(mirosubs.returnURL);
     var myURI = new goog.Uri(window.location);
+    if (goog.DEBUG) {
+        console.log(uri.getDomain());
+        console.log(myURI.getDomain());
+    }
     return uri.getDomain().toLowerCase() != 
         myURI.getDomain().toLowerCase();
 };
 
 mirosubs.isFromDifferentDomain = function() {
+    
     return mirosubs.isEmbeddedInDifferentDomain() || 
         mirosubs.isReturnURLInDifferentDomain();
 };

settings.py
@@ -663,6 +663,6 @@ MEDIA_BUNDLES = {
             ],
         "optimizations": "SIMPLE_OPTIMIZATIONS",
         "closure_deps": "",
-        "include_flash_deps":False,
+        "include_flash_deps": False,
         }
 }