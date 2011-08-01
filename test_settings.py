2dad7cb66b7108ea77839260d10440aeb8c50970
alerion <alerion.um@gmail.com>
2011-08-01 16:34:24 +0300

    fix test_settings

--- test_settings.py
+++ test_settings.py
@@ -16,10 +16,10 @@ HAYSTACK_SOLR_URL = 'http://localhost:8983/solr/core1'
 
 CELERY_ALWAYS_EAGER = True
 
-# INSTALLED_APPS += ('django_nose', )
-# INSTALLED_APPS = list(INSTALLED_APPS)
-# INSTALLED_APPS.remove('mirosubs')
-# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+INSTALLED_APPS += ('django_nose', )
+INSTALLED_APPS = list(INSTALLED_APPS)
+INSTALLED_APPS.remove('mirosubs')
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 
 SITE_ID = 4
