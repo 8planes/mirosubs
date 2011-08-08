07f6f4d86f6f071985fd10d9354d3fa0bf16cac6
Adam Duston <aduston@gmail.com>
2011-08-08 16:50:16 -0400

    Parameterized settings file for running tests from hudson_test.sh

--- hudson_test.sh
+++ hudson_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py test api auth comments messages profiles search statistic teams videos widget --settings=test_settings --with-xunit
+python manage.py test api auth comments messages profiles search statistic teams videos widget --settings=$1 --with-xunit
