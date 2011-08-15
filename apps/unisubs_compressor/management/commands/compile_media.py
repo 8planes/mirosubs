651c71353fe33dafec98bb2b3613ca980b724622
Arthur Debert <arthur@stimuli.com.br>
2011-08-14 22:44:14 -0300

    And fixed typo

--- apps/unisubs_compressor/management/commands/compile_media.py
+++ apps/unisubs_compressor/management/commands/compile_media.py
@@ -206,7 +206,7 @@ class Command(BaseCommand):
         for filename in NO_UNIQUE_URL:
             to_path =  os.path.join(settings.MEDIA_ROOT, filename)
             from_path = os.path.join(final_path, filename)
-            if os.path.exsits(to_path):
+            if os.path.exists(to_path):
                 os.remove(to_path)
             shutil.copy(from_path, to_path)
         
